786bec256d6c2cf4f5b07b506701162b70007436
Alex Folombas <folombas@yandex.ru>
2024-09-18 01:38:01 +0300

    Uploaded class HttpClients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
 public class Main {
     public static void main(String[] args) {
         String url = "https://api.nasa.gov/planetary/apod?api_key=rcWswoo2rOLnTtbx3H9bZHfnTVv4GpbNvwp20PbH";
     }
+
+    HttpClients
 }
