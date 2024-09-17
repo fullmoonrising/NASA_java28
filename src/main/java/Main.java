4d0162e8e00bb200966f0b621df53c8b73eaf226
Alex Folombas <folombas@yandex.ru>
2024-09-18 01:34:00 +0300

    Added NASA Api Key

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,5 @@
 public class Main {
+    public static void main(String[] args) {
+        String url = "https://api.nasa.gov/planetary/apod?api_key=rcWswoo2rOLnTtbx3H9bZHfnTVv4GpbNvwp20PbH";
+    }
 }
