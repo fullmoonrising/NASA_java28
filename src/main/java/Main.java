b2457814d4159d959de7718c0bd4818bfb756095
Alex Folombas <folombas@yandex.ru>
2024-09-18 01:44:55 +0300

    Import Class Httpget

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 
@@ -6,5 +7,7 @@ public class Main {
         String url = "https://api.nasa.gov/planetary/apod?api_key=rcWswoo2rOLnTtbx3H9bZHfnTVv4GpbNvwp20PbH";
 
        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+       HttpGet request = new HttpGet(url);
     }
 }
